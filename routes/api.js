27da7d15a043e86fcc918670f6b2682fb3e626d3
lauphern <lau.p.hern@hotmail.com>
2020-08-12 01:17:42 +0200

    put route (update) implemented

--- routes/api.js
+++ routes/api.js
@@ -29,6 +29,10 @@ module.exports = function (app) {
 
     .post(function (req, res) {
       const project = req.params.project;
+      if (!req.body.issue_title || !req.body.issue_text || !req.body.created_by) {
+        res.status(500).send("Fill in all the required inputs please");
+        throw new Error("Fill in the required inputs");
+      }
       const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
       const newIssue = {
         issue_title,
@@ -40,10 +44,6 @@ module.exports = function (app) {
         created_on: new Date(),
         updated: new Date(),
       };
-      if (!req.body.issue_title || !req.body.issue_text || !req.body.created_by) {
-        res.status(500).send("Fill in all the required inputs please");
-        throw new Error("Fill in the required inputs");
-      }
       MongoClient.connect(CONNECTION_STRING, (err, db) => {
         if (err) throw new Error("Couldn't connect to the database");
         else {
@@ -67,37 +67,40 @@ module.exports = function (app) {
 
     .put(function (req, res) {
       const project = req.params.project;
-      const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
-      const newIssue = {
+      if (!req.body._id || !ObjectId.isValid(req.body._id)) {
+        res.status(500).send("Fill in all the required inputs and provide a valid _id please");
+        throw new Error("Fill in the required inputs and provide a valid _id");
+      }
+      let { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = req.body;
+      if(open === "false") open = false;
+      const updateObj = {
         issue_title,
         issue_text,
         created_by,
         assigned_to,
         status_text,
-        open: true,
-        created_on: new Date(),
+        open,
         updated: new Date(),
       };
-      if (!req.body.issue_title || !req.body.issue_text || !req.body.created_by) {
-        res.status(500).send("Fill in all the required inputs please");
-        throw new Error("Fill in the required inputs");
+      for(let key in updateObj) {
+        if(updateObj[key] === "" || updateObj[key] === undefined) delete updateObj[key]
+      }
+      if(Object.keys(updateObj).length <= 1) {
+        res.status(500).send("Please provide a field to update");
+        throw new Error("No updated field sent");
       }
       MongoClient.connect(CONNECTION_STRING, (err, db) => {
         if (err) throw new Error("Couldn't connect to the database");
         else {
           db.collection(project)
-            .insertOne(newIssue)
-            .then(doc => {
-              return db
-                .collection(project)
-                .findOne({ _id: doc.insertedId })
-                .then(doc => {
-                  res.json(doc);
-                });
+            .findOneAndUpdate({_id: new ObjectId(_id)}, { $set: updateObj })
+            .then(result => {
+              if (!result.lastErrorObject.updatedExisting) throw new Error();
+              else res.json("succesfully updated");
             })
             .catch(err => {
-              res.status(500).send("Something went wrong! The issue couldn't be created");
-              throw new Error("Couldn't create the issue");
+              res.status(500).send("Something went wrong! The issue couldn't be updated");
+              throw new Error(`Could not update ${_id}`);
             });
         }
       });
@@ -115,8 +118,7 @@ module.exports = function (app) {
           db.collection(project)
             .findOneAndDelete({ _id: new ObjectId(req.body._id) })
             .then(result => {
-              console.log(result)
-              if(!result.value) throw new Error()
+              if (!result.value) throw new Error();
               else res.json(`deleted ${result.value._id}`);
             })
             .catch(err => {
